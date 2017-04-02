207d4115cafdaea4c69b48f739f676add366c0f7
RenatoPassos <renato.passos.santos@gmail.com>
2017-04-02 20:07:00 -0300

    Acrescentando os projetos do Módulo 1
    Projetos do Módulo 1 da Maratona Xamarin de Janeiro a Abril de 2017. O
    módulo 1 está quase finalizado.

---
+++ mod-01/XelloXamarinAndroid/XelloXamarinAndroid/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XelloXamarinAndroid
+{
+    [Activity(Label = "XelloXamarinAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
